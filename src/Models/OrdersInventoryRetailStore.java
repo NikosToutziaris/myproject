fb4c62e5a2550b11c84a68a9c37a1f2d4ae27a5b
NikosToutziaris <ntoutziaris@athtech.gr>
2020-10-11 23:52:16 +0300

    OnlineCustomer.java

---
+++ src/Models/OrdersInventoryRetailStore.java
@@ -0,0 +1,13 @@
+package Models;
+
+public class OrdersInventoryRetailStore extends InventoryRetailStore{
+
+    public void createOrder(Customer, Product){
+
+    }
+
+    public  void executeOrder(){
+
+    }
+
+}
